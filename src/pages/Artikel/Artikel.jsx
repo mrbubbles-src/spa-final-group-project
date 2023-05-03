c3b9305a0b95a1ad6c6b06fd4a8a371108199bdf
katharina-groller <Katharina.Groller@dci.education>
2023-05-03 16:11:55 +0200

    wissenswertes für artikel seite geschrieben und cards angefangen,um die cards multifunktional zu machen

---
+++ src/pages/Artikel/Artikel.jsx
@@ -0,0 +1,43 @@
+import CardContainer from "../../components/CardContainer/CardContainer";
+import ArtikelCard from "../../components/ArtikelCard/ArtikelCard";
+
+const Artikel = () => {
+    return (
+        <>
+            <div className="text_container">
+                <h2 id="mid_head">Wissenswertes</h2>
+                <p>
+                    Aktuelle Giftköderwarnungen für Hamburg finden Sie auf
+                    <a
+                        href="https://dogorama.app/de-de/giftkoeder/Hamburg/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {" "}
+                        DOGORAMA
+                    </a>
+                </p>
+                <p>
+                    Wichtige und nützliche Beiträge für Sie und Ihre Haustiere
+                    finden Sie auf dieser Seite.
+                </p>
+                <p>
+                    Wir sind stets engagiert neue, wichtige und spannende
+                    Beiträge für Sie hier zusammenzutragen.
+                </p>
+                <p>
+                    Für weitere spannende und nützliche Informationen schauen
+                    Sie auch unter unserem Reiter
+                    <a href="">Links</a>.
+                </p>
+            </div>
+
+            <CardContainer>
+                <p>hay</p>
+                <ArtikelCard />
+            </CardContainer>
+        </>
+    );
+};
+
+export default Artikel;
