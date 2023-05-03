a05f8e3e29308c6e72f2b4a222ef245b8003aa85
jacqueline-s-w <https://github.com/jacqueline-s-w/git-liste.git>
2023-05-03 18:15:13 +0200

    Zwischen

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "./scss/app.scss";
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
+import Impressum from "./pages/Impressum/Impressum";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <Route path="/" element={<SharedLayout />}>
                         <Route index element={<Home />} />
                         {/* AN DIESER STELLE ZWISCHEN HOME UND NOTFOUND DIE ANDEREN ROUTEN ZU DEN SEITEN EINFÜGEN! */}
+                        <Route path="impressum" element={<Impressum />} />
                         <Route path="*" element={<NotFound />} />
                     </Route>
                 </Routes>
