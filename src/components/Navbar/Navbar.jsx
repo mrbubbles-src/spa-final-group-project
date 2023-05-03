cad8d854044ea23faf374d78ecc2004db31cf011
mrbubbles-src <feaver5683@gmail.com>
2023-05-03 17:19:35 +0200

    navbar funktioniert

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,5 +1,94 @@
+import { NavLink } from "react-router-dom";
+import Menu from "../Menu/Menu";
+
 const Navbar = () => {
-    return <nav>hier könnte ihre nav stehen</nav>;
+    return (
+        <nav className="navigation">
+            <Menu />
+            <ul className="nav-link-container">
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Startseite
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Leistungen
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Artikel
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Galerie
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Über die Praxis
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Kontakt & Anfahrt
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Rhodos
+                    </NavLink>
+                </li>
+                <li className="list-elements">
+                    <NavLink
+                        to="/"
+                        className={({ isActive }) =>
+                            isActive ? "link active" : "link"
+                        }
+                    >
+                        Links
+                    </NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default Navbar;
--- src/context/PraxisProvider.jsx
+++ src/context/PraxisProvider.jsx
@@ -3,8 +3,16 @@ import React from "react";
 export const PraxisContext = React.createContext();
 
 const PraxisProvider = ({ children }) => {
+    const navToggle = (toggle) => {
+        const navLinkContainer = document.querySelector(".nav-link-container");
+        if (toggle) return (navLinkContainer.style.display = "block");
+        else return (navLinkContainer.style.display = "none");
+    };
+
     return (
-        <PraxisContext.Provider value={{}}>{children}</PraxisContext.Provider>
+        <PraxisContext.Provider value={{ navToggle }}>
+            {children}
+        </PraxisContext.Provider>
     );
 };
 
