f5ae6bf1fdb9b54db7ab6049f577c5716211c25d
metin <metinmete.arsu@dci.education>
2023-05-04 10:30:19 +0200

    v_1

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,21 @@ import "./scss/app.scss";
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home/Home";
 import NotFound from "./pages/NotFound/NotFound";
+import Kontakt from "./pages/Kontakt/Kontakt";
 
 function App() {
     return (
         <PraxisProvider>
             <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<SharedLayout />}>
+                    <Route path='/' element={<SharedLayout />}>
                         <Route index element={<Home />} />
                         {/* AN DIESER STELLE ZWISCHEN HOME UND NOTFOUND DIE ANDEREN ROUTEN ZU DEN SEITEN EINFÜGEN! */}
-                        <Route path="*" element={<NotFound />} />
+                        <Route
+                            path='kontakt'
+                            element={<Kontakt />}
+                        />
+                        <Route path='*' element={<NotFound />} />
                     </Route>
                 </Routes>
             </BrowserRouter>
