33480da373a414ef3e6a4d0083694c7d60c26fc0
katharina-groller <Katharina.Groller@dci.education>
2023-05-04 11:19:41 +0200

    refresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import PraxisProvider from "./context/PraxisProvider";
 import "./scss/app.scss";
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home/Home";
+import Artikel from "./pages/Artikel/Artikel";
 import NotFound from "./pages/NotFound/NotFound";
+// import ArtikelCard from "./components/ArtikelCard/ArtikelCard";
 
 function App() {
     return (
@@ -13,6 +15,8 @@ function App() {
                     <Route path="/" element={<SharedLayout />}>
                         <Route index element={<Home />} />
                         {/* AN DIESER STELLE ZWISCHEN HOME UND NOTFOUND DIE ANDEREN ROUTEN ZU DEN SEITEN EINFÜGEN! */}
+                        <Route path="/artikel" element={<Artikel />} />
+
                         <Route path="*" element={<NotFound />} />
                     </Route>
                 </Routes>
