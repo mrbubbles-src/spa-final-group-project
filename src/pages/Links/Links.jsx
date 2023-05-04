89a23534ea06d714ff34f1eb35e2089d82a0c08b
MuhammedTinmaz <muhammed.tinmaz@dci.education>
2023-05-04 14:25:46 +0200

    inhalt eingefügt

---
+++ src/pages/Links/Links.jsx
@@ -0,0 +1,63 @@
+// import React from 'react';
+
+const Links = () => {
+    return (
+        <>
+            <article>
+                <h2 id="mid_head">Links</h2>
+
+                <p>
+                    Hier haben wir einige informative Links in alphabetischer
+                    Reihenfolge für Sie zusammengestellt.
+                </p>
+                <div>
+                    <ul>
+                        <li>
+                            <a href="https://www.dght.de/startseite">
+                                Deutschen Gesellschaft für Herpetologie und
+                                Terrarienkunde (DGHT)
+                            </a>
+                            Die Website der Deutschen Gesellschaft für
+                            Herpetologie und Terrarienkunde
+                        </li>
+                        <li>
+                            <a href="https://www.bundestieraerztekammer.de/tieraerzte/beruf/got/">
+                                GOT
+                            </a>
+                            Gebührenordnung für Tierärzte
+                        </li>
+                        <li>
+                            <a href="http://www.terrarien-freunde-hamburg.de/">
+                                Terrarienfreunde Hamburg
+                            </a>
+                            Die Website der Terrarienfreunde Hamburg
+                        </li>
+                        <li>
+                            <a href="https://www.tasso.net/">Tiernotruf.org</a>
+                            Die Website von TASSO e.V.
+                        </li>
+                        <li>
+                            <a href="http://www.tierschutzbund.de/00524.html">
+                                Tierschutzbund
+                            </a>
+                            Die Website des Deutschen Tierschutzbundes e.V
+                        </li>
+                        <li>
+                            <a href="https://www.agila.de/?VermittlerID=11&gclid=Cj0KCQiA1sucBhDgARIsAFoytUtaV3FPyB5cAf9xXKu7m32abyoYxq0VWqoM3iraqP9b34WgCtdtj6QaAkk6EALw_wcB">
+                                Tierversicherung
+                            </a>
+                            AGILA
+                        </li>
+                        <li>
+                            <a href="https://www.comfortplan.de/">
+                                Tierversicherung Versicherungsvergleich
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </article>
+        </>
+    );
+};
+
+export default Links;
