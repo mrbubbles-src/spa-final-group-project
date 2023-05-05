8f379220ebae6248f888b159e422107b1f4ff979
mrbubbles-src <feaver5683@gmail.com>
2023-05-05 11:08:48 +0200

    removed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PraxisProvider from "./context/PraxisProvider";
+// Styling
 import "./scss/app.scss";
+import "./pages/Home/home.scss";
 import "./pages/Galerie/galerie.scss";
+
+// Pages
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home/Home";
 import Artikel from "./pages/Artikel/Artikel";
 import NotFound from "./pages/NotFound/NotFound";
-// import ArtikelCard from "./components/ArtikelCard/ArtikelCard";
 import Kontakt from "./pages/Kontakt/Kontakt";
 import Impressum from "./pages/Impressum/Impressum";
 import DataProtection from "./pages/DataProtection/DataProtection";
 import Links from "./pages/Links/Links";
 import Galerie from "./pages/Galerie/Galerie";
-import "./pages/Home/home.scss";
 import Leistungen from "./pages/Leistungen/Leistungen";
-
 
 function App() {
     return (
