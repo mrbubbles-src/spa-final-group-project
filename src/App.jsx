4b4628f079c3feb768ed34d6ee67369142bcfed3
mrbubbles-src <feaver5683@gmail.com>
2023-05-05 21:52:56 +0200

    import gallerycontainer.scss

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import PraxisProvider from "./context/PraxisProvider";
 import "./components/Navbar/nav.scss";
 import "./components/Herosection/herosection.scss";
 import "./components/CardContainer/cardContainer.scss";
+import "./components/GalleryContainer/gallerycontainer.scss";
 import "./components/ArtikelCard/artikelCard.scss";
 import "./components/Footer/footer.scss";
 import "./components/Visitenkarte/visitenkarte.scss";
