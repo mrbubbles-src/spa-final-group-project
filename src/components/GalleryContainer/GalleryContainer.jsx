c56ba6a58a1798d16c03b88c1240c3f4a9f9360b
mrbubbles-src <feaver5683@gmail.com>
2023-05-05 22:31:06 +0200

    funktioniert fast und styling muss man fixen

--- src/components/GalleryContainer/GalleryContainer.jsx
+++ src/components/GalleryContainer/GalleryContainer.jsx
@@ -20,111 +20,57 @@ const GalleryContainer = ({ tierGalerie }) => {
 
     return (
         <>
-            <div className="modal-window">
-                {openModal && (
-                    <div className="modal-container">
-                        <FontAwesomeIcon
-                            icon={faCircleXmark}
-                            className="close-modal-image"
-                            onClick={handleCloseModal}
+            {openModal && (
+                <div className="modal-container">
+                    <FontAwesomeIcon
+                        icon={faCircleXmark}
+                        className="close-modal-image"
+                        onClick={handleCloseModal}
+                    />
+                    <FontAwesomeIcon
+                        icon={faAngleLeft}
+                        className="prev-modal-image"
+                        onClick={() => handlePrevSlide(tierGalerie)}
+                    />
+                    <FontAwesomeIcon
+                        icon={faAngleRight}
+                        className="next-modal-image"
+                        onClick={() => handleNextSlide(tierGalerie)}
+                    />
+                    <div className="fullscreen-image-container">
+                        <img
+                            className="fullscreen-image"
+                            src={tierGalerie[slidePosition].img}
+                            alt={tierGalerie[slidePosition].alt}
                         />
-                        <FontAwesomeIcon
-                            icon={faAngleLeft}
-                            className="prev-modal-image"
-                            onClick={() => handlePrevSlide(tierGalerie)}
-                        />
-                        <FontAwesomeIcon
-                            icon={faAngleRight}
-                            className="next-modal-image"
-                            onClick={() => handleNextSlide(tierGalerie)}
-                        />
-                        <div className="fullscreenImage">
-                            <img
-                                src={tierGalerie[slidePosition].img}
-                                alt={tierGalerie[slidePosition].alt}
-                            />
-                        </div>
                     </div>
-                )}
-                <div className="gallery-image-container">
-                    {tierGalerie &&
-                        tierGalerie.map((currImage, index) => {
-                            return (
-                                <div
-                                    className="single-gallery-image"
-                                    key={index}
-                                    onClick={() => handleOpenModal(index)}
-                                >
-                                    <img
-                                        src={
-                                            currImage.img
-                                                ? currImage.img
-                                                : NoImage
-                                        }
-                                        alt={
-                                            currImage.alt
-                                                ? currImage.alt
-                                                : "Kein Bild gefunden"
-                                        }
-                                    />
-                                </div>
-                            );
-                        })}
                 </div>
-            </div>
+            )}
 
-            {/* <div className="modal-window">
-                {openModal && (
-                    <div className="modal-container">
-                        <FontAwesomeIcon
-                            icon={faCircleXmark}
-                            className="close-modal-image"
-                            onClick={handleCloseModal}
-                        />
-                        <FontAwesomeIcon
-                            icon={faAngleLeft}
-                            className="prev-modal-image"
-                            onClick={() => handlePrevSlide(praxisGalerie)}
-                        />
-                        <FontAwesomeIcon
-                            icon={faAngleRight}
-                            className="next-modal-image"
-                            onClick={() => handleNextSlide(praxisGalerie)}
-                        />
-                        <div className="fullscreenImage">
-                            <img
-                                src={praxisGalerie[slidePosition].img}
-                                alt={praxisGalerie[slidePosition].alt}
-                            />
-                        </div>
-                    </div>
-                )}
-                <div className="gallery-image-container">
-                    {praxisGalerie &&
-                        praxisGalerie.map((currImage, index) => {
-                            return (
-                                <div
+            <div className="gallery-image-container">
+                {tierGalerie &&
+                    tierGalerie.map((currImage, index) => {
+                        return (
+                            <div
+                                className="single-gallery-image-container"
+                                key={index}
+                                onClick={() => handleOpenModal(index)}
+                            >
+                                <img
                                     className="single-gallery-image"
-                                    key={index}
-                                    onClick={() => handleOpenModal(index)}
-                                >
-                                    <img
-                                        src={
-                                            currImage.img
-                                                ? currImage.img
-                                                : NoImage
-                                        }
-                                        alt={
-                                            currImage.alt
-                                                ? currImage.alt
-                                                : "Kein Bild gefunden"
-                                        }
-                                    />
-                                </div>
-                            );
-                        })}
-                </div>
-            </div> */}
+                                    src={
+                                        currImage.img ? currImage.img : NoImage
+                                    }
+                                    alt={
+                                        currImage.alt
+                                            ? currImage.alt
+                                            : "Kein Bild gefunden"
+                                    }
+                                />
+                            </div>
+                        );
+                    })}
+            </div>
         </>
     );
 };
