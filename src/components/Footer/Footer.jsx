7789aa2e1874d279a64bfacdb1a29f5ad2382706
mrbubbles-src <feaver5683@gmail.com>
2023-05-05 11:35:39 +0200

    removed shit

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,21 +1,11 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import {
-//     faTwitter,
-//     faInstagram,
-//     faFacebook,
-// } from "@fortawesome/free-brands-svg-icons";
-// import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
         <footer>
-            {/* <div className="footer_icons">
-                <FontAwesomeIcon className="icons" icon={faTwitter} />
-                <FontAwesomeIcon className="icons" icon={faInstagram} />
-                <FontAwesomeIcon className="icons" icon={faFacebook} />
-            </div> */}
-            <p className="footer_copy">&copy; 2023 Dr.med.vet. Gerlitzki</p>
+            <p className="footer_copy paragraph">
+                &copy; 2023 Dr.med.vet. Gerlitzki
+            </p>
             <div className="footer_links">
                 <Link to="/impressum">Impressum</Link>{" "}
                 <Link to="/dataprotection">Datenschutz</Link>
