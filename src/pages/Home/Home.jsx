c1f4b67067be36198ace95b9bf9c60012e2c0c81
mrbubbles-src <feaver5683@gmail.com>
2023-05-06 18:10:09 +0200

    mobile stying landing page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,37 +5,31 @@ const Home = () => {
     return (
         <>
             <section className="welcome">
-                <div className="text_container_top">
-                    <div className="text_container">
-                        <h2 className="sub-head">
-                            Herzlich Willkommen in unserer Tierarztpraxis!
-                        </h2>
-                        <p className="paragraph">
-                            In unserer Praxis gehören Empathie und
-                            überdurchschnittliches medizinisches Niveau
-                            zusammen. Unser Hauptanliegen ist das Wohl des
-                            Tieres, weshalb wir unnötige Untersuchungen und
-                            somit unnötige Zusatzkosten und Stress des Tieres
-                            vermeiden.
-                        </p>
-                        <p className="paragraph">
-                            Damit Sie als Besitzer und Vertrauter ihrem
-                            tierischen Begleiter bei der Genesung helfen können,
-                            erklären wir Ihnen möglichst genau, das
-                            Krankheitsbild, mit seinen Ursachen, Verlauf und die
-                            benötigten Maßnahmen, um möglichst stressfrei und
-                            erfolgreich zur Genesung beizutragen.
-                            <br />
-                            <br />
-                            Bei weiteren Fragen oder Sorgen, sprechen Sie uns
-                            einfach an. Wir helfen Ihnen gerne weiter. <br />
-                            <br />
-                            Ihr Dr. Volker Gerlitzki und Team.
-                        </p>
-                    </div>
-                    <div className="circle">
-                        <img src={AceHallo} />
-                    </div>
+                <h2 className="sub-head">
+                    Herzlich Willkommen in unserer Tierarztpraxis!
+                </h2>
+                <p className="paragraph">
+                    In unserer Praxis gehören Empathie und
+                    überdurchschnittliches medizinisches Niveau zusammen. Unser
+                    Hauptanliegen ist das Wohl des Tieres, weshalb wir unnötige
+                    Untersuchungen und somit unnötige Zusatzkosten und Stress
+                    des Tieres vermeiden.
+                    <br />
+                    <br />
+                    Damit Sie als Besitzer und Vertrauter ihrem tierischen
+                    Begleiter bei der Genesung helfen können, erklären wir Ihnen
+                    möglichst genau, das Krankheitsbild, mit seinen Ursachen,
+                    Verlauf und die benötigten Maßnahmen, um möglichst
+                    stressfrei und erfolgreich zur Genesung beizutragen.
+                    <br />
+                    <br />
+                    Bei weiteren Fragen oder Sorgen, sprechen Sie uns einfach
+                    an. Wir helfen Ihnen gerne weiter. <br />
+                    <br />
+                    Ihr Dr. Volker Gerlitzki und Team.
+                </p>
+                <div className="welcome-circle-picture">
+                    <img className="welcome-image" src={AceHallo} />
                 </div>
             </section>
 
