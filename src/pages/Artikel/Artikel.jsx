2fc542e1c6491159d9a0622ab7cf62df47f9f831
mrbubbles-src <feaver5683@gmail.com>
2023-05-06 19:49:36 +0200

    aufgeräumt und gestyled

--- src/pages/Artikel/Artikel.jsx
+++ src/pages/Artikel/Artikel.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
 import ArtikelCard from "../../components/ArtikelCard/ArtikelCard";
 
 const Artikel = () => {
     return (
         <>
-            <div className="text_container">
+            <section className="articles-section">
                 <h2 className="sub-head">Wissenswertes</h2>
-                <p className="paragraph">
+                <p className="article-paragraph paragraph">
                     Aktuelle Giftköderwarnungen für Hamburg finden Sie auf
                     <a
                         href="https://dogorama.app/de-de/giftkoeder/Hamburg/"
@@ -16,24 +17,24 @@ const Artikel = () => {
                         {" "}
                         DOGORAMA
                     </a>
-                </p>
-                <p className="paragraph">
+                    <br />
+                    <br />
                     Wichtige und nützliche Beiträge für Sie und Ihre Haustiere
                     finden Sie auf dieser Seite.
-                </p>
-                <p className="paragraph">
+                    <br />
+                    <br />
                     Wir sind stets engagiert neue, wichtige und spannende
                     Beiträge für Sie hier zusammenzutragen.
-                </p>
-                <p className="paragraph">
+                    <br />
+                    <br />
                     Für weitere spannende und nützliche Informationen schauen
                     Sie auch unter unserem Reiter
-                    <a href="" className="site-links">
+                    <Link to="/links" className="site-links">
                         Links
-                    </a>
+                    </Link>
                     .
                 </p>
-            </div>
+            </section>
 
             <ArtikelCard />
         </>
