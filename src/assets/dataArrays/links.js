28c578d7f1ec1c69ae39e8188488a893b84f9ad0
mrbubbles-src <feaver5683@gmail.com>
2023-05-07 19:04:11 +0200

    links array erstellt

---
+++ src/assets/dataArrays/links.js
@@ -0,0 +1,31 @@
+export const links = [
+    {
+        link: "https://www.dght.de/startseite",
+        linkText:
+            "Deutschen Gesellschaft für Herpetologie und Terrarienkunde (DGHT)",
+    },
+    {
+        link: "https://www.bundestieraerztekammer.de/tieraerzte/beruf/got/",
+        linkText: "Gebührenordnung für Tierärzte (GOT)",
+    },
+    {
+        link: "http://www.terrarien-freunde-hamburg.de/",
+        linkText: "Terrarienfreunde Hamburg",
+    },
+    {
+        link: "https://www.tasso.net/",
+        linkText: "TASSO e.V. (Tiernotruf)",
+    },
+    {
+        link: "http://www.tierschutzbund.de/00524.html",
+        linkText: "Deutscher Tierschutzbund e.V",
+    },
+    {
+        link: "https://www.agila.de/?VermittlerID=11&gclid=Cj0KCQiA1sucBhDgARIsAFoytUtaV3FPyB5cAf9xXKu7m32abyoYxq0VWqoM3iraqP9b34WgCtdtj6QaAkk6EALw_wcB",
+        linkText: "AGILA Tierversicherung",
+    },
+    {
+        link: "https://www.comfortplan.de/tierversicherung",
+        linkText: "Comfortplan Tierversicherungsvergleich",
+    },
+];
