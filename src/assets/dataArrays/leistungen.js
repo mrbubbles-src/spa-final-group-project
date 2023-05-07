271e6b5a88bd698b21c00d0fff8684a5856c0d32
mrbubbles-src <feaver5683@gmail.com>
2023-05-07 15:13:31 +0200

    erstellt

---
+++ src/assets/dataArrays/leistungen.js
@@ -0,0 +1,52 @@
+export const leistungen = [
+    {
+        title: "Diagnostik",
+        itemOne: "Röntgendiagnostik",
+        itemTwo: "Ultraschall-Untersuchungen",
+        itemThree: "Laboruntersuchungen",
+        itemFour: "EKG",
+        itemFive: "Allergietests und Desensibilisierung",
+        itemSix: "Geriatrie-Check",
+    },
+    {
+        title: "Physiotherapeutische Anwendungen",
+        itemOne: "nach orthopädischen Operationen (z.B. zur Schmerztherapie)",
+        itemTwo: "Revitalisierung im Bewegungsapparat (Muskelaufbau)",
+        itemThree:
+            "zur Motivation der Bewegung bei älteren Tieren oder zur Gewichtsreduktion",
+    },
+    {
+        title: "Züchterspezifische Analysen",
+        itemOne: "DNA-Bestimmungen",
+        itemTwo: "Blutgruppen-Bestimmung",
+        itemThree: "Reihenuntersuchen",
+    },
+    {
+        title: "Weitere Leistungen",
+        itemOne: "Augenheilkunde",
+        itemTwo: "Zahnheilkunde",
+        itemThree: "Elektronische Tierkennzeichnung/EU Heimtierpässe",
+        itemFour: "Tierärztliche Vollapotheke",
+        itemFive: "Große Auswahl an Diätfuttermitteln",
+        itemSix: "Impferinnerung",
+        itemSeven: "EC-Kartenzahlung",
+        itemEight: "Hunde- und Katzenfriseur",
+    },
+    {
+        title: "Beratung",
+        itemOne: "Beratung bei Verhaltensproblemen",
+        itemTwo: "Ernährungsberatung",
+    },
+    {
+        title: "Operationen und Chirurgie",
+        itemOne: "Injektions-, Inhalations- und Kurznarkosen",
+        itemTwo: "Weichteil- und Knochenchirurgie",
+    },
+    {
+        title: "Hausbesuche",
+        itemOne:
+            "RöntIn Notfällen,bei Bestandsbetreuungen oder Fällen einer Transportunfähigkeit des Patienten halten wir auch Hausbesuche ab. Eine weitergehende Untersuchung und Behandlung des Patienten ist jedoch häufig nur in der Praxis möglich.gendiagnostik",
+        itemTwo:
+            "Bitte haben Sie Verständnis, dass unser “normaler” Sprechstunden- und Operationsbetrieb erstrangig abgesichert werden muss.",
+    },
+];
