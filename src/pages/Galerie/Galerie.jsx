b1bf90d3670a3866484c15252f564cf750ec1691
mrbubbles-src <feaver5683@gmail.com>
2023-05-08 15:39:32 +0200

    useEffect zur praxis sec scroll

--- src/pages/Galerie/Galerie.jsx
+++ src/pages/Galerie/Galerie.jsx
@@ -1,8 +1,20 @@
 import GalleryContainer from "../../components/GalleryContainer/GalleryContainer";
 import { tiere } from "../../assets/imageArrays/galerieBilderTiere";
 import { praxis } from "../../assets/imageArrays/galerieBilderPraxis";
+import { useLocation } from "react-router-dom";
+import { useEffect } from "react";
 
 const Galerie = () => {
+    const praxisLocation = useLocation();
+    useEffect(() => {
+        if (praxisLocation.hash) {
+            const praxisSection = document.querySelector(praxisLocation.hash);
+            if (praxisSection) {
+                praxisSection.scrollIntoView({ behavior: "smooth" });
+            }
+        }
+    }, [praxisLocation]);
+
     return (
         <>
             <h2 className="galerie-sub-title sub-head">Galerie</h2>
@@ -10,7 +22,7 @@ const Galerie = () => {
                 <h3 className="galerie-section-title">Unsere Patienten</h3>
                 <GalleryContainer galerie={tiere} galleryId="tiere" />
             </section>
-            <section className="galerie-container">
+            <section className="galerie-container" id="praxis-gal">
                 <h3 className="galerie-section-title">Unsere Praxis</h3>
                 <GalleryContainer galerie={praxis} galleryId="praxis" />
             </section>
