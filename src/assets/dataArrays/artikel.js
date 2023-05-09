5530823e5ed5e7c18c8f07950452945c4fb5f554
mrbubbles-src <feaver5683@gmail.com>
2023-05-09 23:31:12 +0200

    aaaa

--- src/assets/dataArrays/artikel.js
+++ src/assets/dataArrays/artikel.js
@@ -1,7 +1,7 @@
 export const articleAbout = [
     {
         title: "Zahnmedizin",
-        image: "./public/images/tiere/Katze-offener-Mund-SMALL.jpg",
+        image: "/images/tiere/Katze-offener-Mund-SMALL.jpg",
         alt: "katze mit offenem mund",
         information: `Zahnschmerzen können uns Tiere leider nicht mitteilen. Allerdings können regelmäßige Prophilaxe, Plaquekontrollen als Vorsorgemaßnahme, Zahnerkrankungen und dadurch bedingte Folgeschäden weitesgehend vorgebeugt und zusätzlicher Stress für das Tier und Kosten minimiert werden.\nAblagerungen an den Zähnen, unangenehmer Mundgeruch, stärkerer Speichelfluss als normal, schlechtere Kautätigkeit, sie Zahnfleischentzündungen, sind eindeutige Anzeichen für Zahnprobleme. Darauf folgen Parandontose, Karies und eventuell auch entzündungen, die bei Vernachlässigung dem gesamten Organismus schdaen können und Leber, Herz und Niere angreifen können.\nEntzündungen können als Folge von Zahnbelägen und Zahnstein (Beläge aus Bakterien, die durch Einlagerung von Mineralstoffen steinhart werden) entstehen und zusätzlich auch zu zahnverlust führen, bei unregelmäßger Kontrolle und Behandlung.\nNeben regelmäßgen tierärztlichen Kontrollen, können Sie ihrem Tier durch tägliche, gründliche Reinigung der Zähne mit speziellen Zahnbürsten (im Zoohandel/Tierbedarf), Zahnpasta oder auch Zahnreinigungsstreifen und entspechendem Trockenfutter, zu gesunden Zähnen verhelfen.`,
     },
