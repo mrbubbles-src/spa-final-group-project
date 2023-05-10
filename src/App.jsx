b66f42444edfa0cd855ce423d8fccf8a83a4facd
mrbubbles-src <feaver5683@gmail.com>
2023-05-10 09:59:22 +0200

    removed imports and unnecessary comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import PraxisProvider from "./context/PraxisProvider";
-// styling
 import "./components/Navbar/nav.scss";
 import "./components/Herosection/herosection.scss";
 import "./components/GalleryContainer/gallerycontainer.scss";
@@ -8,14 +7,12 @@ import "./components/Visitenkarte/visitenkarte.scss";
 import "./components/ReadMore/readMore.scss";
 import "./pages/Home/home.scss";
 import "./pages/Leistungen/leistungen.scss";
-import "./pages/Galerie/galerie.scss";
 import "./pages/Praxis/praxis.scss";
 import "./pages/Kontakt/kontakt.scss";
 import "./pages/Impressum/impressum.scss";
 import "./pages/DataProtection/dataProtection.scss";
 import "./pages/NotFound/notFound.scss";
 import "./scss/app.scss";
-// pages
 import SharedLayout from "./pages/SharedLayout";
 import Home from "./pages/Home/Home";
 import Leistungen from "./pages/Leistungen/Leistungen";
